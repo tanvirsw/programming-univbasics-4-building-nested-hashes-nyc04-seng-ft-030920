de19b405862c8f410dc77f70ab79626f3c1df0ef
Tanvir Walia <tarywalia@gmail.com>
2020-02-24 20:11:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,14 +15,43 @@ end
 def monopoly_with_third_tier
   monopoly ={
     railroads:{
-      :names =>{
+      pieces: 4,
+      names: {
         :reading_railroad => {},
         :pennsylvania_railroad =>{},
         :b_and_o_railroad => {},
         :shortline => {},
-
         }
-      :pieces => 4,
+      rent_in_dollars: {
+        :one_piece_owned=>25,
+        :two_pieces_owned=>50,
+        :three_pieces_owned=>100,
+        :four_pieces_owned=>200,
+      }
+    }
+  }
+end
+
+def monopoly_with_fourth_tier
+  monopoly ={
+    railroads:{
+      :names =>{
+        :reading_railroad => {
+          "mortgage_value" => "$100"
+        },
+        :pennsylvania_railroad =>{
+          "mortgage_value" => "$200"
+        },
+        :b_and_o_railroad => {
+          "mortgage_value" => "$400"
+        },
+        :shortline => {
+          "mortgage_value" => "$800"
+        },
+
+      }
+      pieces: 4,
+
       :rent_in_dollars => {
         :one_piece_owned=>25,
         :two_pieces_owned=>50,
