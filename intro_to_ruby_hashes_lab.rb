447092d7fae8a54555be210d725e4a6a7d36f04f
Tanvir Walia <tarywalia@gmail.com>
2020-02-24 20:16:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,7 +48,7 @@ def monopoly_with_fourth_tier
           :b_and_o_railroad => {
             "mortgage_value" => "$400"
           },
-          :shortline => {
+          :shortline_railroad => {
             "mortgage_value" => "$800"
           }
       },
