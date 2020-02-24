174da7503f068087e205d0aea2fe1e4cf3cedd04
Tanvir Walia <tarywalia@gmail.com>
2020-02-24 20:08:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
         :b_and_o_railroad => {},
         :shortline => {},
 
-      
+      }
       :pieces => 4,
       :rent_in_dollars => {
         :one_piece_owned=>25,
